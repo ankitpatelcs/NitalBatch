ebfd99645329cb9fe270354881dde510637e89b9
Ankit <ankitpatel.tops@gmail.com>
2022-07-25 09:33:11 +0530

    Razor Controls 2

--- RazorControls/RazorControls/Controllers/DefaultController.cs
+++ RazorControls/RazorControls/Controllers/DefaultController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,14 +16,54 @@ namespace RazorControls.Controllers
         {
             return View(dc.tblemployees.ToList());
         }
+
+        void fillcity()
+        {
+            var citydata = from c in dc.tblcities
+                           select new SelectListItem
+                           {
+                               Text = c.city_name,
+                               Value = c.city_id.ToString()
+                           };
+            ViewBag.cities = citydata.ToList();
+        }
         public ActionResult Create()
         {
+            fillcity();
             return View();
         }
         [HttpPost]
-        public ActionResult Create(tblemployee obj)
+        public ActionResult Create(tblemployee obj, FormCollection fc, HttpPostedFileBase file)
         {
-            return View();
+            bool Reading = Convert.ToBoolean(fc["Reading"].Split(',')[0]);
+            bool Playing = Convert.ToBoolean(fc["Playing"].Split(',')[0]);
+            bool Listening = Convert.ToBoolean(fc["Listening"].Split(',')[0]);
+            string hoby = "";
+            if (Reading)
+            {
+                hoby += "Reading,";
+            }
+            if (Playing)
+            {
+                hoby += "Playing,";
+            }
+            if (Listening)
+            {
+                hoby += "Listening,";
+            }
+            obj.hobbies = hoby;
+
+            if (file!=null)
+            {
+                string filename = Path.GetFileName(file.FileName);
+                string fullpath = Path.Combine(Server.MapPath("~/Docs"),filename);
+                file.SaveAs(fullpath);
+                obj.profileimg = filename;
+            }
+
+            dc.tblemployees.Add(obj);
+            dc.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
