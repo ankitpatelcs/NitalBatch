5de5364d6ce04411cc17f8a57fa635a984f01366
Ankit Patel <76809891+ankittops301@users.noreply.github.com>
2022-07-28 09:19:38 +0530

    2

--- ProjectDemo/ProjectDemo/Areas/Admin/Controllers/MasterController.cs
+++ ProjectDemo/ProjectDemo/Areas/Admin/Controllers/MasterController.cs
@@ -3,15 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProjectDemo.EDM;
 
 namespace ProjectDemo.Areas.Admin.Controllers
 {
     public class MasterController : Controller
     {
+        ecommerceEntities dc = new ecommerceEntities();
         // GET: Admin/Master
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection fc)
+        {
+            string email = fc["email"];
+            string pass = fc["password"];
+
+            var obj = dc.tblAdmins.Where(u => u.email == email && u.password == pass).FirstOrDefault();
+            if (obj != null)
+            {
+                Session["AdminId"] = obj.Admin_id;
+                Session["AdminName"] = obj.f_name;
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                ViewBag.loginerror = "Invalid Email or Password.!";
+            }
+
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login");
+        }
+        
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProduct(tblproduct obj)
+        {
+
+            dc.tblproducts.Add(obj);
+            dc.SaveChanges();
+            return RedirectToAction("ManageProducts");
+        }
+        public ActionResult ManageProducts()
+        {
+            return View(dc.tblproducts.ToList());
         }
     }
 }
