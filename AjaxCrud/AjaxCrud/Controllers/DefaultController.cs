e8d0d1c8f6fb3146cab434fbbaa351d78baa1a36
Ankit Patel <76809891+ankittops301@users.noreply.github.com>
2022-08-03 08:56:47 +0530

    ajax

---
+++ AjaxCrud/AjaxCrud/Controllers/DefaultController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxCrud.EDM;
+
+namespace AjaxCrud.Controllers
+{
+    public class DefaultController : Controller
+    {
+        CompanyEntities dc = new CompanyEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEmployees()
+        {
+            dc.Configuration.ProxyCreationEnabled = false;
+            return Json(dc.tblemployees.ToList(), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Create(tblemployee obj)
+        {
+            dc.tblemployees.Add(obj);
+            dc.SaveChanges();
+            return "Create Success.";
+        }
+        public ActionResult Edit(int id)
+        {
+            return View(dc.tblemployees.Find(id));
+        }
+        [HttpPost]
+        public string Edit(tblemployee obj)
+        {
+            dc.Entry(obj).State = System.Data.Entity.EntityState.Modified;
+            dc.SaveChanges();
+            return "Update Success.";
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(dc.tblemployees.Find(id));
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public string DeleteRec(int id)
+        {
+            dc.tblemployees.Remove(dc.tblemployees.Find(id));
+            dc.SaveChanges();
+            return "Delete Success.";
+        }
+    }
+}
--- ProjectDemo/ProjectDemo/Areas/Users/Controllers/DefaultController.cs
+++ ProjectDemo/ProjectDemo/Areas/Users/Controllers/DefaultController.cs
@@ -50,5 +50,26 @@ namespace ProjectDemo.Areas.Users.Controllers
         {
             return View();
         }
+        public ActionResult Products()
+        {
+            return View(dc.tblproducts.ToList());
+        }
+        public ActionResult Single(int id)
+        {
+            return View(dc.tblproducts.Find(id));
+        }
+
+        public string AddToCart(int id)
+        {
+            int userid = Convert.ToInt32(Session["UserId"]);
+            tblcart obj = new tblcart();
+            obj.product_id = id;
+            obj.qty = 1;
+            obj.user_id = userid;
+
+            dc.tblcarts.Add(obj);
+            dc.SaveChanges();
+            return "Product added to Cart.";
+        }
     }
 }
