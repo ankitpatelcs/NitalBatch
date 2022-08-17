52f023880fba0e35cba2108090a1d17d173c98a2
Ankit Patel <76809891+ankittops301@users.noreply.github.com>
2022-08-17 10:55:10 +0530

    ado crud

---
+++ ADOCrud/ADOCrud/Controllers/DefaultController.cs
@@ -0,0 +1,59 @@
+﻿using ADOCrud.Models;
+using ADOCrud.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ADOCrud.Controllers
+{
+    public class DefaultController : Controller
+    {
+        IEmployeeOperations dc = new EmployeeOperations();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View(dc.GetEmployees());
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(dc.GetEmployeeById(id));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(tblemployee obj)
+        {
+            dc.InsertEmployee(obj);
+            return RedirectToAction("Index");
+        }
+        
+        public ActionResult Edit(int id)
+        {
+            return View(dc.GetEmployeeById(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(tblemployee obj)
+        {
+            dc.UpdateEmployee(obj);
+            return RedirectToAction("Index");
+        }
+        
+        public ActionResult Delete(int id)
+        {
+            return View(dc.GetEmployeeById(id));
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteRec(int id)
+        {
+            dc.DeleteEmployee(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ADOCrud/ADOCrud/Services/IEmployeeOperations.cs
@@ -0,0 +1,18 @@
+﻿using ADOCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOCrud.Services
+{
+    interface IEmployeeOperations
+    {
+        List<tblemployee> GetEmployees();
+        tblemployee GetEmployeeById(int id);
+        void InsertEmployee(tblemployee obj);
+        void UpdateEmployee(tblemployee obj);
+        void DeleteEmployee(int id);
+    }
+}
